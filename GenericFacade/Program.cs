221fcf874a854178e72078f15aa1e973aa0b06c4
Luciano Frutuoso de Brito <britojam@hotmail.com>
2021-06-21 15:22:41 -0300

    Add Interface Facade Pattern

--- GenericFacade/Program.cs
+++ GenericFacade/Program.cs
@@ -7,16 +7,24 @@ namespace GenericFacade
     {
         static void Main(string[] args)
         {
-            ServiceA serviceA = new ServiceA();
-            int sAResult = serviceA.Method2();
-            
-            ServiceB serviceB = new ServiceB();
-            string sBResult = serviceB.Method2();
-            
-            ServiceC serviceC = new ServiceC();
-            double sCResult = serviceC.Method1();
-            
-            Console.WriteLine(sAResult + " - " + sCResult + " - " + sBResult);
+            //ServiceA serviceA = new ServiceA();
+            //int sAResult = serviceA.Method2();
+
+            //ServiceB serviceB = new ServiceB();
+            //string sBResult = serviceB.Method2();
+
+            //ServiceC serviceC = new ServiceC();
+            //double sCResult = serviceC.Method1();
+
+           // Console.WriteLine(sAResult + " - " + sCResult + " - " + sBResult);
+
+            IServiceFacade facade = new ServiceFacade();
+
+            Tuple<int, double, string> result = facade.CallFacade();
+
+            Console.WriteLine(result.Item1 + " - " + result.Item2 + " - " + result.Item3);
+
+           
             
         }
     }
---
+++ GenericFacade/Services/IServiceFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericFacade.Services
+{
+    public interface IServiceFacade
+    {
+        Tuple<int, double, string> CallFacade();
+    }
+}

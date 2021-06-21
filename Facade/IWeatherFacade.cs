9ab0eb47668a8ccf5ba6af8ff2895a3e0db8bbd8
Luciano Frutuoso de Brito <britojam@hotmail.com>
2021-06-21 14:24:55 -0300

    Add Facede Interface to weather

---
+++ Facade/IWeatherFacade.cs
@@ -0,0 +1,50 @@
+﻿using Facade.Entities;
+using Facade.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public interface IWeatherFacade
+    {
+        WeatherFacadeResults GetTempCity(string zipCode);
+    }
+
+    public class WeatherFacade : IWeatherFacade
+    {
+        private readonly ConverterService _converterService;
+        private readonly GeoLookupService _geoLookupService;
+        private readonly WeatherService _weatherService;
+
+        public WeatherFacade() : this(new ConverterService(), new GeoLookupService(), new WeatherService())
+        {
+
+        }
+
+        public WeatherFacade(ConverterService converterService, GeoLookupService geoLookupService, WeatherService weatherService)
+        {
+            _converterService = converterService;
+            _geoLookupService = geoLookupService;
+            _weatherService = weatherService;
+        }
+
+        public WeatherFacadeResults GetTempCity(string zipCode)
+        {
+            City city = _geoLookupService.GetCityForZipCode(zipCode);
+            State state = _geoLookupService.GetStateForZipCode(zipCode);
+            int tempF = _weatherService.GetTempFahrenheit(city, state);
+            int tempC = _converterService.ConvertFahrenheitToCelcious(tempF);
+
+            var results = new WeatherFacadeResults
+            {
+                City = city,
+                State = state,
+                Fahrenheit = tempF,
+                Celsius = tempC
+            };
+
+            return results;
+        }
+    }
+}
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -9,26 +9,36 @@ namespace Facade
         static void Main(string[] args)
         {
             const string zipCode = "98074";
-            
-            // call to service 1
-            GeoLookupService geoLookupService = new GeoLookupService();
-            City city = geoLookupService.GetCityForZipCode(zipCode);
-            State state = geoLookupService.GetStateForZipCode(zipCode);
-            
-            // call to service 2
-            WeatherService weatherService = new WeatherService();
-            int fahrenheit = weatherService.GetTempFahrenheit(city, state);
-            
-            // call to service 3
-            ConverterService metricConverter = new ConverterService();
-            int celcius = metricConverter.ConvertFahrenheitToCelcious(fahrenheit);
-            
-            // bring the result of all service calls together
-            Console.WriteLine("The current temperature is {0} F / {1} C in {2}, {3}",
-                                fahrenheit,
-                                celcius,
-                                city.Name,
-                                state.Name);
+
+            // Refactory to Facade Pattern
+            //// call to service 1
+            //GeoLookupService geoLookupService = new GeoLookupService();
+            //City city = geoLookupService.GetCityForZipCode(zipCode);
+            //State state = geoLookupService.GetStateForZipCode(zipCode);
+
+            //// call to service 2
+            //WeatherService weatherService = new WeatherService();
+            //int fahrenheit = weatherService.GetTempFahrenheit(city, state);
+
+            //// call to service 3
+            //ConverterService metricConverter = new ConverterService();
+            //int celcius = metricConverter.ConvertFahrenheitToCelcious(fahrenheit);
+
+            //// bring the result of all service calls together
+            //Console.WriteLine("The current temperature is {0} F / {1} C in {2}, {3}",
+            //                    fahrenheit,
+            //                    celcius,
+            //                    city.Name,
+            //                    state.Name);
+
+            IWeatherFacade weatherFacade = new WeatherFacade();
+            WeatherFacadeResults results = weatherFacade.GetTempCity(zipCode);
+            Console.WriteLine("The currnt temperature is {0}F/{1}C in {2}, {3}",
+                results.Fahrenheit,
+                results.Celsius,
+                results.City.Name,
+                results.State.Name
+            );
 
         }
     }
